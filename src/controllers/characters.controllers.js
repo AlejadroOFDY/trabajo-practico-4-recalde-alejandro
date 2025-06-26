dd5e86569e96bbdc6c3e47558c6f2e4d1a39f1af
AlejandroOFDY <BB_Rae@hotmail.com>
2025-06-26 14:13:58 -0300

    Terminados character.(routes y controllers)

--- src/controllers/characters.controllers.js
+++ src/controllers/characters.controllers.js
@@ -1,5 +1,8 @@
 import Characters from "../models/character.model.js";
 
+// Acá va a ir lo del CRUD (Create, Read, Update, Delete)
+
+ // Obtener los personajes
 export const getAllCharacters = async (req, res) => {
     try {
         const characters = await Characters.findAll();
@@ -9,10 +12,12 @@ export const getAllCharacters = async (req, res) => {
     }
 };
 
+// Obtener 1 personaje
 export const getCharacterById = async (req, res) => {
     try {
         const character = Characters.findByPk(req.params.id);
         // No olvidar que ! es el operador de NEGACIÓN
+        // Validación
         if (!character) {
             return res.status(404).json({error: "Personaje no encontrado"});
         }
@@ -21,6 +26,7 @@ export const getCharacterById = async (req, res) => {
     }
 };
 
+// Crear pjes
 export const createCharacter = async (req, res) => {
     try {
         const {name, ki, race, gender, description } = req.body; //req.body es la info enviada por el usuario
@@ -52,6 +58,49 @@ export const createCharacter = async (req, res) => {
     }
 };
 
+// Modificar un pje
 export const updateCharacter = async (req, res) => {
-    
-}
+    try {
+        const character = Characters.findByPk(req.params.id);
+        // Validaciones
+        if (!character) {
+            return res.status(404).json({ error: "No se encontró el personaje"});
+        }
+
+        const { name, ki, race, gender, description } = req.body;
+
+        if (name && (await Characters.findOne({ where: { name } }))) {
+            return res.status(400).json({error: "El nombre ya está en uso"});
+        }
+        if (ki && isNaN(ki)) {
+            res.status(400).json({ error: "El ki debe ser un número entero"})
+        }
+        if (gender !== "Male" && gender !== "Female") {
+            res.status(400).json({ error: "El género solo puede ser Male o Female"});
+        }
+        await character.update({
+            name: name || character.name,
+            ki: ki ? parseInt(ki) : character.ki,
+            race: race || character.race,
+            gender: gender || character.gender,
+            description: description || character.description,
+        });
+        res.status(200).json(character);
+    } catch {error} {
+        res.status(500).json({ error: "Error al actualizar el personaje"});
+    }
+};
+
+// Eliminar pjes
+export const deleteCharacter = async (req, res) => {
+    try {
+        const character = await Characters.findByPk(req.params.id);
+        if (!character) {
+            return res.status(404).json({ error: "No se encontró el personaje"});
+        }
+        await character.destroy();
+        res.status(200).json({message: "Se eliminó el personaje exitosamente"});
+    } catch (error) {
+        res.status(500).json({error: "No se pudo eliminar el personaje"})
+    }
+};
--- src/routes/character.routes.js
+++ src/routes/character.routes.js
@@ -4,5 +4,16 @@ import {
     getCharacterById,
     createCharacter,
     updateCharacter,
+    deleteCharacter,
 
-} from "../routes/character.controllers.js"
+} from "../routes/character.controllers.js";
+
+const router = Router();
+
+router.get("/", getAllCharacters);
+router.get("/:id", getCharacterById);
+router.get("/", createCharacter);
+router.get("/:id", updateCharacter);
+router.get("/:id", deleteCharacter);
+
+export default router;
