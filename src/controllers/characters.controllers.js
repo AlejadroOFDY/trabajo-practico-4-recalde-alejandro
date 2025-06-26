2fb8e03a17bc6fb79de3ea1bedc007bea1f541f4
AlejandroOFDY <BB_Rae@hotmail.com>
2025-06-26 10:45:11 -0300

    Intercambiando los códigos de characters.(routes y controllers) a como deben ser

--- src/controllers/characters.controllers.js
+++ src/controllers/characters.controllers.js
@@ -1,5 +1,16 @@
-import { Router } from "express";
-import {
-    todosLosPersonajes,
-    
+import personajeDB from "../models/character.model.js";
+
+export const todosLosPersonajes = async (req, res) => {
+    try {
+        const personajes = await personajeDB.findAll();
+        res.status(200).json(personajes)
+    } catch (error) {
+        res.status(500).json({error: "No se pudo obtener todos los personajes"})
+    }
+};
+
+export const peronajePorID = async (req, res) => {
+    try {
+        
+    }
 }
--- src/routes/character.routes.js
+++ src/routes/character.routes.js
@@ -1,14 +1,5 @@
-import personajeDB from "../models/character.model.js";
+import { Router } from "express";
+import {
+    todosLosPersonajes,
 
-export const todosLosPersonajes = async (req, res) => {
-    try {
-        const personajes = await personajeDB.findAll();
-        res.status(200).json(personajes)
-    } catch (error) {
-        res.status(500).json({error: "No se pudo obtener todos los personajes"})
-    }
-};
-
-export const peronajePorID = async (req, res) => {
-    
-}
+} from "../routes/character.controllers.js"
