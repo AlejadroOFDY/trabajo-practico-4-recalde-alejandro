ed752ffc26871513dca553f9fd932875e17f57c8
Alejandro <BB_rae@hotmail.com>
2025-06-26 01:49:32 -0300

    Subiendo la estructura y conidciones de la BD

--- src/models/character.model.js
+++ src/models/character.model.js
@@ -2,6 +2,23 @@ import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
 
 // Creando los objetos
-const personaje = sequelize.define("Personaje", {
-  name: {},
+const personajeDB = sequelize.define("Personaje", {
+  name: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+    unique: true, // unique es para evitar duplicados
+  },
+  ki: { type: DataTypes.STRING, allowNull: false },
+  race: { type: DataTypes.STRING, allowNull: false },
+  gender: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    isIn: [["Male", "Female"]],
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true, // True porque esto es opcional, a diferencia de name, ki o race
+  },
 });
+
+export default personajeDB;
